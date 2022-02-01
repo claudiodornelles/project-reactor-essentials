9d141d41d0dcda80ffb3a86402b390e48ef8e839
Claudio Dornelles <dornelles.crtd@gmail.com>
2022-02-01 16:18:11 -0300

    Mono subscribe basics

--- src/test/java/com/claudiodornelles/reactive/test/MonoTest.java
+++ src/test/java/com/claudiodornelles/reactive/test/MonoTest.java
@@ -1,9 +1,11 @@
 package com.claudiodornelles.reactive.test;
 
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscription;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 /*
  * Reactive Streams (Pattern)
@@ -26,8 +28,73 @@ class MonoTest {
     @Test
     void monoSubscriber() {
         String name = "Claudio Dornelles";
-        Mono<String> mono = Mono.just(name);
-        log.info("Mono {}", mono);
+        Mono<String> mono = Mono.just(name).log();
+
+        mono.subscribe();
+
+        StepVerifier.create(mono)
+                .expectNext(name)
+                .verifyComplete();
+    }
+
+    @Test
+    void monoSubscriberConsumer() {
+        String name = "Claudio Dornelles";
+        Mono<String> mono = Mono.just(name).log();
+
+        mono.subscribe(string -> log.info("Stream Value: {}", string));
+
+        StepVerifier.create(mono)
+                .expectNext(name)
+                .verifyComplete();
+    }
+
+    @Test
+    void monoSubscriberConsumerError() {
+        String name = "Claudio Dornelles";
+        Mono<String> mono = Mono.just(name)
+                .map(string -> {
+                    throw new RuntimeException("Testing mono with error");
+                });
+
+        mono.subscribe(string -> log.info("Stream Value: {}", string), error -> log.error("Something went wrong", error));
+
+        StepVerifier.create(mono)
+                .expectError(RuntimeException.class)
+                .verify();
+    }
+
+    @Test
+    void monoSubscriberConsumerComplete() {
+        String name = "Claudio Dornelles";
+        Mono<String> mono = Mono.just(name).log().map(String::toUpperCase);
+
+        mono.subscribe(
+                string -> log.info("Stream Value: {}", string),
+                error -> log.error("Somenthing went wrong", error),
+                () -> log.info("Stream complete")
+        );
+
+        StepVerifier.create(mono)
+                .expectNext(name.toUpperCase())
+                .verifyComplete();
+    }
+
+    @Test
+    void monoSubscriberConsumerCompleteSubscription() {
+        String name = "Claudio Dornelles";
+        Mono<String> mono = Mono.just(name).log().map(String::toUpperCase);
+
+        mono.subscribe(
+                string -> log.info("Stream Value: {}", string),
+                error -> log.error("Somenthing went wrong", error),
+                () -> log.info("Stream complete"),
+                Subscription::cancel
+        );
+
+        StepVerifier.create(mono)
+                .expectNext(name.toUpperCase())
+                .verifyComplete();
     }
 }
 
